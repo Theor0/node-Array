a23374b6c0fe8b5d9615021d8dd0fbc69ef681ed
Theor0 <theohop0@gmail.com>
2022-06-02 09:17:04 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-bzmicp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-bzmicp)
---
+++ index.js
@@ -0,0 +1,13 @@
+let contas = new Array('Moto');
+
+contas.push('mesa');
+contas.push('telefone');
+contas.push('fonte');
+contas.push('academia');
+contas.push('viagem');
+contas.unshift('tiaGi');
+//contas.pop()
+//contas.splice(5, 1);
+
+console.table(contas);
+console.log(contas.length);
